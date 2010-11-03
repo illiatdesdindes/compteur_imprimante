8a975dc1f69c2aa5d4224df6ff7ffe7df5736f6a
illiatdesdindes <illiatdesdindes@gmail.com>
2010-11-03 22:06:15 +0100

    Comptes.py ok

--- comptes.py
+++ comptes.py
@@ -1,22 +1,36 @@
 
 class Comptes:
-	def __init__(self, dataFile):
-		self.comptes={}
-		self.dataFile = dataFile
-		
-		with open(dataFile, 'r') as fluxdata:
-			contenu = fluxdata.readlines()
-		for ligne in contenu:
-			ligne = ligne.strip
-			self.comptes.append(User(data = ligne).nom, User(data = ligne))
+    def __init__(self, dataFile):
+        self.comptes={}
+        self.dataFile = dataFile
+
+        with open(dataFile, 'r') as fluxdata:
+            contenu = fluxdata.readlines()
+        for ligne in contenu:
+            ligne = ligne.strip(' \n\r')
+            try:
+                user = User(data = ligne)
+                self.comptes[User(data = ligne).nom] = User(data = ligne)
+            except TypeError:
+                pass #evite d'enregistrer des lignes vides
+
 
     def ajoute(self, nom, compteur = 0):
-        comptes[nom] = User(nom, compteur)
-    
+        self.comptes[nom] = User(nom, compteur)
+
     def enregistre(self):
-        with open(dataFile, 'w') as fluxdata:
-            
-    def 
+        with open(self.dataFile, 'w') as fluxdata:
+            fluxdata.write(str(self))
+
+    def __str__(self):
+        contenu = ''
+        for user in self.comptes.values():
+            contenu += user.data+'\n'
+        return contenu
+
+    def __repr__(self):
+        return self.comptes
+
 class User:
     nbUser = 0
 
@@ -31,9 +45,9 @@ class User:
         else:
             raise TypeError('mauvaise initialisation: ni nom, ni data n\' ete fourni')
         User.nbUser += 1
-    
+
     def majData(self):
-        self.data = self.serialise()        
+        self.data = self.serialise()
 
     def majAttr(self):
         self.nom, self.compteur = self.deserialise()
@@ -43,29 +57,19 @@ class User:
 
     def deserialise(self):
         return self.data.split(':')
-    
+
     def incr(self, x = 1):
         self.compteur += x
         self.majData()
 
     def __del__(self):
         User.nbUser -= 1
-    
+
     def __str__(self):
         return self.data
-    
+
     def __repr__(self):
         return "{0} - {1} - {2}".format(self.nom,
                                         self.compteur,
                                         self.data)
 
-
-
-
-
-
-
-
-
-
-    
--- test.py
+++ test.py
@@ -1,15 +1,9 @@
 from comptes import *
 
-#c = Comptes('compteur.list')
-#print c.comptes
-#c.ajoute('iren')
-#print c.comptes
-#c.supprime(bob)
-#print c.comptes
-#c.enregistre()
-#print User.nbUser
+c = Comptes('compteur.list')
+print c
+c.ajoute('jack')
+c.ajoute('boris',2)
+print c
+c.enregistre()
 
-a = [User('bob'),User('mec',5), User(data='fred:10')]
-
-for i in a:
-    print repr(i)

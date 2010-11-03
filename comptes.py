398e71f5d4c1a779eee45cc7c0ade77a86bc1bad
illiatdesdindes <illiatdesdindes@gmail.com>
2010-11-03 21:17:27 +0100

    User ok

--- comptes.py
+++ comptes.py
@@ -1,21 +1,27 @@
 
-class Comptes
+class Comptes:
 	def __init__(self, dataFile):
 		self.comptes={}
+		self.dataFile = dataFile
 		
 		with open(dataFile, 'r') as fluxdata:
 			contenu = fluxdata.readlines()
 		for ligne in contenu:
 			ligne = ligne.strip
-			self.comptes.append(User(data = ligne).nom,
-								User(data = ligne))		
+			self.comptes.append(User(data = ligne).nom, User(data = ligne))
 
+    def ajoute(self, nom, compteur = 0):
+        comptes[nom] = User(nom, compteur)
+    
+    def enregistre(self):
+        with open(dataFile, 'w') as fluxdata:
+            
+    def 
 class User:
-	
-	nbUser = 0
-	
-	def __ini__(self, nom ='' , compteur=0, data=''):
-		if nom:
+    nbUser = 0
+
+    def __init__(self, nom = '', compteur = 0, data = ''):
+        if nom:
             self.nom = nom
             self.compteur = compteur
             self.majData()
@@ -23,17 +29,17 @@ class User:
             self.data = data
             self.majAttr()
         else:
-            raise TypeError('mauvaise initialisation: ni nom, ni data n\' ete\
-                             fourni')
+            raise TypeError('mauvaise initialisation: ni nom, ni data n\' ete fourni')
+        User.nbUser += 1
     
     def majData(self):
-        self.nom, self.compteur = self.deserialise()
+        self.data = self.serialise()        
 
     def majAttr(self):
-        self.data = self.serialise()
+        self.nom, self.compteur = self.deserialise()
 
     def serialise(self):
-        return '{0}:{1}'.format(self.nom, str(self.compteur))
+        return '{0}:{1}'.format(self.nom, self.compteur)
 
     def deserialise(self):
         return self.data.split(':')
@@ -42,7 +48,16 @@ class User:
         self.compteur += x
         self.majData()
 
-
+    def __del__(self):
+        User.nbUser -= 1
+    
+    def __str__(self):
+        return self.data
+    
+    def __repr__(self):
+        return "{0} - {1} - {2}".format(self.nom,
+                                        self.compteur,
+                                        self.data)
 
 
 
--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 from comptes import *
 
-c = Comptes('compteur.list')
-print c.comptes
-c.ajoute('iren')
-print c.comptes
-c.supprime(bob)
-print c.comptes
-c.enregistre()
+#c = Comptes('compteur.list')
+#print c.comptes
+#c.ajoute('iren')
+#print c.comptes
+#c.supprime(bob)
+#print c.comptes
+#c.enregistre()
 #print User.nbUser
+
+a = [User('bob'),User('mec',5), User(data='fred:10')]
+
+for i in a:
+    print repr(i)

c8e9dbf56b714535388cdcf4d3641b09693b29f1
illiatdesdindes <illiatdesdindes@gmail.com>
2010-11-04 00:54:56 +0100

    gui de base ok

--- compteur.py
+++ compteur.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 
-LISTE = "compteur.liste"
+from Tkinter import *
+from comptes import *
+
+LISTE = "compteur.list"
 VERROU = "compteur.lock"
+LASTUSER = "lastuser"
 
 #-------VERROU---------------------------------
 
@@ -9,7 +13,7 @@ def verrou ():
 	"""Verifie l'état du verrou"""
 	with open(VERROU, "r") as fluxLock:
 		etat = fluxLock.readline()
-	if etat == "unlock":
+	if etat == "uself.nlock":
 		return False
 	else:
 		return True
@@ -24,6 +28,41 @@ def verrouUnlock():
 	with open(VERROU, 'w') as fluxLock:
 	    fluxLock.write("unlock")
 
+def lastUser():
+    """retourn le dernier utilisateur"""
+    with open(LASTUSER,'r') as flux:
+        contenu = flux.read().strip(' \n\r')
+    if contenu == '':
+        return 'Entrez un nom d\'utilisateur'
+    else :
+        return contenu
+
+class MyFrame(Frame):
+    def __init__(self, titre):
+        Frame.__init__(self)
+        self.master.title(titre)
+
+        comptes = Comptes(LISTE)
+
+        self.message = Label(self, text='Bonjour').pack()
+
+        boiteh1 = Frame(self)
+        boiteh1Texte = Label(boiteh1, text='Liste des utilisateurs :').pack(side="left", padx=5)
+        self.liste = Label(boiteh1, text=str(comptes)).pack(side='right', padx=10)
+        boiteh1.pack()
+
+        self.user = Entry(self, value=lastUser()).pack()
+
+        self.compteur = Entry(self, value='1').pack()
+
+        self.ok = Button(self, text='Ok').pack()
+
+        self.pack()
+
+
 
 if __name__ == "__main__":
+    app = MyFrame("Compteur de copies")
+
+    app.mainloop()
 
